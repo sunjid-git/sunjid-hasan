c353030eed2168f3626797188b2e66b4d033770c
sunjid-git <sunjid.info@gmail.com>
2021-09-15 13:37:32 +0600

    project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,197 @@
 import React from "react";
 import "./Projects.css";
 
+import project1 from "../../images/project1.PNG";
+import project2 from "../../images/project2.PNG";
+import project3 from "../../images/project3.PNG";
+
 export default function Projects() {
   return (
     <>
       <section className="projects info-background">
-        <div>mafi</div>
+      <h1 className="heading">Projects</h1>
+        <div className="project-cards">
+          <div className="project-card">
+            <img src={project1} alt="project1" />
+            <h4>Bengal It Plus</h4>
+            <p>
+              An online store with multiple service option and functionality
+              like dynamic data loading with relevant information and dynamic
+              review section.
+            </p>
+
+           <div>
+           <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="stay-in-touch"
+            >
+              Visit GitHub
+            </a>
+
+            <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="resume"
+            >
+              Visit Website
+            </a>
+           </div>
+          </div>
+
+          <div className="project-card">
+            <img src={project2} alt="project1" />
+            <h4>Bengal It Plus</h4>
+            <p>
+              An online store with multiple service option and functionality
+              like dynamic data loading with relevant information and dynamic
+              review section.
+            </p>
+
+            <div>
+           <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="stay-in-touch"
+            >
+              Visit GitHub
+            </a>
+
+            <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="resume"
+            >
+              Visit Website
+            </a>
+           </div>
+          </div>
+
+          <div className="project-card">
+            <img src={project3} alt="project1" />
+            <h4>Bengal It Plus</h4>
+            <p>
+              An online store with multiple service option and functionality
+              like dynamic data loading with relevant information and dynamic
+              review section.
+            </p>
+
+            <div>
+           <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="stay-in-touch"
+            >
+              Visit GitHub
+            </a>
+
+            <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="resume"
+            >
+              Visit Website
+            </a>
+           </div>
+          </div>
+
+          <div className="project-card">
+            <img src={project3} alt="project1" />
+            <h4>Bengal It Plus</h4>
+            <p>
+              An online store with multiple service option and functionality
+              like dynamic data loading with relevant information and dynamic
+              review section.
+            </p>
+
+            <div>
+           <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="stay-in-touch"
+            >
+              Visit GitHub
+            </a>
+
+            <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="resume"
+            >
+              Visit Website
+            </a>
+           </div>
+          </div>
+
+          <div className="project-card">
+            <img src={project3} alt="project1" />
+            <h4>Bengal It Plus</h4>
+            <p>
+              An online store with multiple service option and functionality
+              like dynamic data loading with relevant information and dynamic
+              review section.
+            </p>
+
+            <div>
+           <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="stay-in-touch"
+            >
+              Visit GitHub
+            </a>
+
+            <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="resume"
+            >
+              Visit Website
+            </a>
+           </div>
+          </div>
+
+          <div className="project-card">
+            <img src={project3} alt="project1" />
+            <h4>Bengal It Plus</h4>
+            <p>
+              An online store with multiple service option and functionality
+              like dynamic data loading with relevant information and dynamic
+              review section.
+            </p>
+
+            <div>
+           <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="stay-in-touch"
+            >
+              Visit GitHub
+            </a>
+
+            <a
+              href="https://sunjid-web.medium.com/commonly-asked-html-interview-questions-81d18f4ef49c"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="resume"
+            >
+              Visit Website
+            </a>
+           </div>
+          </div>
+
+        </div>
       </section>
     </>
   );
